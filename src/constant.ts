b836216f4faf42ca179807c0606ddc13013bd52e
Libaan-H <61713495+Libaan-H@users.noreply.github.com>
2023-03-05 19:57:53 +0000

    Monthly constants

--- src/constant.ts
+++ src/constant.ts
@@ -1,6 +1,8 @@
 // Define constants for rent and VAT
 export const MIN_RENT_WEEKLY = 2500; // in pence
 export const MAX_RENT_WEEKLY = 200000; // in pence
+export const MIN_RENT_MONTHLY = 11000; // in pence
+export const MAX_RENT_MONTHLY = 866000; // in pence
 export const VAT_RATE = 0.2;
 export const MIN_MEMBERSHIP_PRICE: number =
   MIN_RENT_WEEKLY + MIN_RENT_WEEKLY * VAT_RATE;
