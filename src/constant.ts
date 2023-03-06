eac1418eee9d66fa28f82dc477c862dc9b467bb2
Libaan-H <libaan.hassan@outlook.com>
2023-03-06 14:58:16 +0000

    Set fixed variable of min membership fee

--- src/constant.ts
+++ src/constant.ts
@@ -4,5 +4,4 @@ export const MAX_RENT_WEEKLY = 200000; // in pence
 export const MIN_RENT_MONTHLY = 11000; // in pence
 export const MAX_RENT_MONTHLY = 866000; // in pence
 export const VAT_RATE = 0.2;
-export const MIN_MEMBERSHIP_PRICE: number =
-  MIN_RENT_WEEKLY + MIN_RENT_WEEKLY * VAT_RATE;
+export const MIN_MEMBERSHIP_PRICE: number = 12000 + 12000 * VAT_RATE;
